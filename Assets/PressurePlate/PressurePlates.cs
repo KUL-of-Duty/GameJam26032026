f7741d6aaedf8683013cee7aff105e932fe68103
Maksymilian Oszkiel <max.oszkiel@gmail.com>
2026-03-27 15:19:48 +0100

    Dog Polecenia + Fix pressure platow

--- Assets/PressurePlate/PressurePlates.cs
+++ Assets/PressurePlate/PressurePlates.cs
@@ -19,20 +19,9 @@ public class PressurePlates : MonoBehaviour
     {
         Dog = GameObject.FindWithTag("Dog");
     }
-    private void OnTriggerEnter(Collider other)
-    {
-        if (!DogPressure)
-        {
-            Dog.GetComponent<DogBehaviour>().GoToButton(DogPressurePlate);
-            DogPressure = true;
-        }
-    }
-
     public void ActivateDoors()
     {
-        if (!DogPressure) return;
-
-        Dog.GetComponent<DogBehaviour>().TaskDone();
+        if (Vector3.Distance(DogPressurePlate.transform.position, Dog.transform.position) > 5f && Vector3.Distance(Plate.transform.position, Dog.transform.position) > 5f) return;
         Door.SetActive(false);
         Bone.SetActive(false);
     }
