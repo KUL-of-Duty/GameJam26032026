7d128e93618895e26b9a9d0105b331ce8845fe1c
Maksymilian Oszkiel <max.oszkiel@gmail.com>
2026-03-27 13:26:33 +0100

    Cam shake, Enemy

--- Assets/Detector.cs
+++ Assets/Detector.cs
@@ -4,7 +4,7 @@ using UnityEngine.InputSystem;
 public class Detector : MonoBehaviour
 {
     public GameObject detectedItem = null;
-    public float detectionDistance = 10000.0f;
+    public float detectionDistance = 10.0f;
 
     void Update()
     {
--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -1,17 +1,73 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
     public GameObject dogSpot;
     public GameObject dogSpot2;
-    void Start()
+
+    [SerializeField]
+    ParticleSystem[] particles;
+
+    [SerializeField]
+    CameraShake camShake;
+
+    float hitTime;
+    float cooldown = 1f;
+
+    RaycastHit hit;
+
+    public void AtackPlayer()
     {
-        
+        foreach (var particle in particles)
+        {
+            particle.Play();
+            camShake.Shake();
+        }
     }
 
-    // Update is called once per frame
+    private void Awake()
+    {
+        hitTime = Time.time;
+    }
+
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Atack();
+        }
+    }
+
+    void Atack()
+    {
+        if (Time.time - hitTime < cooldown) return;
+        hitTime = Time.time;
+
+        StartCoroutine(AtackCooldowned());
+    }
+
+    IEnumerator AtackCooldowned()
+    {
+        Vector3 origin = transform.position;
+        Vector3 direction = transform.forward;
+
+        if (Physics.Raycast(origin, direction, out hit, 3f))
+        {
+            Debug.Log(hit.collider.gameObject.name);
+
+            if (hit.collider.CompareTag("Enemy"))
+            {
+                Enemy enemy = hit.collider.GetComponent<Enemy>();
+                if (enemy != null)
+                {
+                    yield return new WaitForSeconds(0.5f);
+                    enemy.AtackEnemy();
+                }
+            }
+        }
+
+        Debug.DrawRay(origin, direction * 3f, Color.red, 1f);
     }
 }
